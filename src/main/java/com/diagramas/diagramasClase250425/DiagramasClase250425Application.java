48acc1b5ec7f33850895e9deaa9b1b193ee08248
faste <faste@DESKTOP-GI2BVNN>
2025-04-25 17:01:32 +0200

    main
    primer commit con comentarios

--- src/main/java/com/diagramas/diagramasClase250425/DiagramasClase250425Application.java
+++ src/main/java/com/diagramas/diagramasClase250425/DiagramasClase250425Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiagramasClase250425Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiagramasClase250425Application.class, args);
+		SpringApplication.run(DiagramasClase250425Application.class, args);// ola
 	}
 
 }
