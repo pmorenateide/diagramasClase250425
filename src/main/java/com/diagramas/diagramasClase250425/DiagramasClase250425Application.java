089570daa16a5f084abacc7be9ff9e663815c0ce
pmorenateide <pmorena@teideformacion.com>
2025-04-25 15:50:43 +0200

    Add files via upload

---
+++ src/main/java/com/diagramas/diagramasClase250425/DiagramasClase250425Application.java
@@ -0,0 +1,13 @@
+package com.diagramas.diagramasClase250425;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagramasClase250425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagramasClase250425Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/diagramas/diagramasClase250425/DiagramasClase250425ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagramas.diagramasClase250425;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagramasClase250425ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
